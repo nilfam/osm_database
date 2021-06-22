f01c3c3374790c4ba0c43f5d08be301d6c2c563a
Niloofar Aflaki <naflaki4@gmail.com>
2021-06-22 23:19:13 +1200

    TBC

--- Chart.py
+++ Chart.py
@@ -13,7 +13,18 @@ df3= pd.read_csv("C:\\Users\\naflaki\\Documents\\ArcGisShowCases\\df3.csv")
 
 for i, df in enumerate([df1, df2, df3], 1):
 
-    plt.plot(df['Loc'], df['Dist'], label=f'df{i}', marker=True)
-
+    plt.plot(df['Dist'], df['Fre'], label=f'df{i}', marker=True)
+    plt.scatter(x=df['Dist'], y=df['Fre'])
+    plt.xticks(rotation=90, ha='right')
+    plt.subplots_adjust(bottom=0.4, top=0.99)
 plt.legend()
 plt.show()
+
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# df  = pd.read_csv("C:\\Users\\naflaki\\Documents\\ArcGisShowCases\\df1.csv")
+# df.plot()  # plots all columns against index
+# df.plot(kind='scatter',x='Loc',y='Dist') # scatter plot
+# df.plot(kind='density')  # estimate density function
+# # df.plot(kind='hist')  # histogram
