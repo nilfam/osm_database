ae1d9f5c1a28712400f00517dce5c7f4d062d9b4
Yukio Fukuzawa <nishinokaze8@gmail.com>
2021-08-17 13:03:26 +1200

    Export list of relatum/locatum that have more than 1 match

--- osm_database/management/commands/calculate_nearest_points_with_correction.py
+++ osm_database/management/commands/calculate_nearest_points_with_correction.py
@@ -31,8 +31,21 @@ cache = {}
 polygon_for_buckingham_str = fix_wkt_str_if_necessary('POLYGON ((-0.1414478 51.5004912, -0.1413575 51.5004168, -0.1411859 51.5004769, -0.1412582 51.5005324, -0.1412292 51.5005103, -0.1408867 51.5006834, -0.1408787 51.5006778, -0.1411113 51.500418, -0.1410233 51.5003477, -0.1413553 51.5002015, -0.1415197 51.5001309, -0.1421681 51.4998456, -0.1429618 51.4995133, -0.1430538 51.4994539, -0.1430736 51.4994107, -0.1434675 51.49897, -0.1435577 51.4987364, -0.143735 51.4985128, -0.1437777 51.4985128, -0.1437934 51.4984895, -0.1438107 51.4984687, -0.1438057 51.4984483, -0.1440177 51.4982276, -0.1441394 51.4982036, -0.1444667 51.4982508, -0.1451252 51.4983121, -0.1460595 51.4983121, -0.1466704 51.4982971, -0.1469263 51.4983326, -0.1476788 51.4992781, -0.1479532 51.4995336, -0.1480988 51.4996669, -0.1484046 51.4999764, -0.1490604 51.500372, -0.1496317 51.5007262, -0.1501158 51.501029, -0.151032 51.5016886, -0.1511355 51.5018037, -0.1503356 51.5020898, -0.1497485 51.5023315, -0.1495841 51.5023599, -0.1493132 51.5023783, -0.1488116 51.5023583, -0.1482296 51.5023349, -0.1470065 51.5022814, -0.1449198 51.5022046, -0.1439568 51.5021813, -0.1429935 51.5021297, -0.1428768 51.5020207, -0.1427784 51.5019496, -0.1426426 51.501823, -0.1426323 51.5018272, -0.1425728 51.5017716, -0.142586 51.5017662, -0.1424654 51.501653, -0.1421985 51.5017631, -0.1420925 51.5016636, -0.1421116 51.5016557, -0.1418684 51.5014271, -0.1418405 51.5014386, -0.1417575 51.5013606, -0.1416908 51.5012979, -0.1417206 51.5012856, -0.1414697 51.5010496, -0.141453 51.5010564, -0.141345 51.5009548, -0.1416102 51.5008455, -0.1414989 51.5007408, -0.1414856 51.5007463, -0.141422 51.5006865, -0.1414353 51.500681, -0.1412988 51.5005525, -0.1413536 51.50053, -0.141341 51.5005182, -0.1414058 51.5004915, -0.1414184 51.5005034, -0.1414478 51.5004912))')
 polygon_for_buckingham = shapely.wkt.loads(polygon_for_buckingham_str)
 
-cache['Buckingham Palace'] = [4256976, 'way', polygon_for_buckingham.centroid, polygon_for_buckingham_str]
+cache['Buckingham Palace'] = [None, 4256976, 'way', polygon_for_buckingham.centroid, polygon_for_buckingham_str]
 
+
+if os.path.isfile('suspicious.pkl'):
+    with open('suspicious.pkl', 'rb') as f:
+        suspicious = pickle.load(f)
+else:
+    suspicious = {}
+
+
+def get_suspicous_osm_entities_info(osm_entities):
+    rows = []
+    for osm_entity in osm_entities:
+        rows.append([osm_entity.osm_type, osm_entity.osm_id])
+    return rows
 
 class ExcelRow:
     def get_geoinfo_for_name(self, name, osm_id=None, osm_type=None):
@@ -42,7 +55,9 @@ class ExcelRow:
                     osm_id = 4256976
                 if osm_type is None:
                     osm_type = 'way'
-                return [osm_id, osm_type, polygon_for_buckingham.centroid, polygon_for_buckingham_str]
+                return [None, osm_id, osm_type, polygon_for_buckingham.centroid, polygon_for_buckingham_str]
+            else:
+                return cache[name]
 
         if osm_id is not None and osm_id != '':
             if osm_type is not None and osm_type != '':
@@ -56,15 +71,15 @@ class ExcelRow:
             else:
                 osm_entities = OsmEntity.objects.filter(osm_id=int(osm_id))
         else:
-            osm_entities = OsmEntity.objects.filter(display_name=name)
+            osm_entities = OsmEntity.objects.filter(display_name__startswith=name)
 
             if osm_entities.count() == 0:
                 warning('No osm entity found for {}, use existing info from Excel'.format(name))
-                return None, None, None, None
+                return osm_entities, None, None, None, None
 
             elif osm_entities.count() > 1:
                 warning('More than one OSM entity found for {}, use existing info from Excel'.format(name))
-                return None, None, None, None
+                return osm_entities, None, None, None, None
 
         osm_entity = osm_entities.first()
         osm_id = osm_entity.osm_id
@@ -97,13 +112,13 @@ class ExcelRow:
         else:
             wkt = None
 
-        cache[name] = (osm_id, osm_type, centroid, wkt)
-        return osm_id, osm_type, centroid, wkt
+        cache[name] = (None, osm_id, osm_type, centroid, wkt)
+        return None, osm_id, osm_type, centroid, wkt
 
     def __init__(self, row):
         self.location = row['Locatum']
         if self.location == '':
-            raise Exception('Row is empty')
+            raise RuntimeError('Row is empty')
         self.preposition = row['Preposition']
         self.relatum = row['Relatum']
         self.frequency = row['Fre']
@@ -122,7 +137,8 @@ class ExcelRow:
         rel_osm_type = row['Rel OSMType']
 
         # query for the actual boundary and centroid from the database
-        self.loc_id, self.loc_osm_type, db_loc_centroid, db_loc_wkt = self.get_geoinfo_for_name(self.location, loc_id, loc_osm_type)
+        info = self.get_geoinfo_for_name(self.location, loc_id, loc_osm_type)
+        loc_osm_entities, self.loc_id, self.loc_osm_type, db_loc_centroid, db_loc_wkt = info
 
         if db_loc_centroid is None:
             self.centroid_loc = Point(lat_loc, lon_loc)
@@ -137,7 +153,7 @@ class ExcelRow:
             self.boundary_loc_str = fix_wkt_str_if_necessary(db_loc_wkt)
         self.boundary_loc = shapely.wkt.loads(self.boundary_loc_str)
 
-        self.rel_id, self.rel_osm_type, db_rel_centroid, db_rel_wkt = self.get_geoinfo_for_name(self.relatum, rel_id, rel_osm_type)
+        rel_osm_entities, self.rel_id, self.rel_osm_type, db_rel_centroid, db_rel_wkt = self.get_geoinfo_for_name(self.relatum, rel_id, rel_osm_type)
         if db_rel_centroid is None:
             self.centroid_rel = Point(lat_rel, lon_rel)
         else:
@@ -163,6 +179,20 @@ class ExcelRow:
         self.rel_nearest_boundary_point = None
         self.rel_closest_boundary_point_to_loc_centroid = None
 
+        if loc_osm_entities is not None and loc_osm_entities.count() != 1:
+            if self.location not in suspicious:
+                suspicious[self.location] = {
+                    'all_osm_entities': get_suspicous_osm_entities_info(loc_osm_entities),
+                    'used': [loc_osm_type, loc_id]
+                }
+
+        if rel_osm_entities is not None and rel_osm_entities.count() != 1:
+            if self.relatum not in suspicious:
+                suspicious[self.relatum] = {
+                    'all_osm_entities': get_suspicous_osm_entities_info(rel_osm_entities),
+                    'used': [rel_osm_type, rel_id]
+                }
+
 
 class Command(BaseCommand):
     def __init__(self):
@@ -183,7 +213,7 @@ class Command(BaseCommand):
             for row_num, row in df.iterrows():
                 try:
                     ExcelRow(row)
-                except:
+                except RuntimeError as e:
                     print('Row #{} is empty'.format(row_num))
 
     def populate_objects_from_excel(self, file):
@@ -304,6 +334,31 @@ class Command(BaseCommand):
         finally:
             with open('cache.pkl', 'wb') as f:
                 pickle.dump(self.sheets, f)
+
+        with open('suspicious.pkl', 'wb') as f:
+            pickle.dump(suspicious, f)
+
+        df = pd.DataFrame(columns=['Name', 'Is used', 'Links'])
+        ind = 0
+        for name, info in suspicious.items():
+            used_type, used_id = info['used']
+            used_type = used_type[0].upper()
+
+            link = 'https://nominatim.openstreetmap.org/ui/details.html?osmtype={}&osmid={}'.format(used_type, used_id)
+            row = [name, 'Current', link]
+            df.loc[ind] = row
+            ind += 1
+            all_osm_entities = info['all_osm_entities']
+            for osm_type, osm_id in all_osm_entities:
+                osm_type = osm_type[0].upper()
+                is_used = osm_type == used_type and osm_id == used_id
+                link = 'https://nominatim.openstreetmap.org/ui/details.html?osmtype={}&osmid={}'.format(osm_type,osm_id)
+                row = ['', is_used, link]
+                df.loc[ind] = row
+                ind += 1
+
+        with pd.ExcelWriter('suspicious.xlsx', mode='w') as writer:
+            df.to_excel(writer, startrow=0)
 
         print('Now correct Excel file')
 
